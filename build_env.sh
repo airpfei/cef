fd438e7f056092b4aea9980cf5ed592793451f13
Peng Fei <airpfei@gmail.com>
2014-01-21 17:34:52 +0800

    add build script

---
+++ build_env.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+chmod +x chromium_base/src/third_party/llvm-build/Release+Asserts/bin/clang
+chmod +x chromium_base/src/third_party/llvm-build/Release+Asserts/bin/clang++
+unzip Debug/libcef.dylib.zip -d Debug/
+chmod +x tools/change_mach_o_flags_from_xcode.sh
+chmod 777 tools/change_mach_o_flags.py
+chmod 777 tools/change_mach_o_flags_from_xcode.sh
+chmod 777 tools/strip_from_xcode
+chmod +x tools/make_more_helpers.sh
